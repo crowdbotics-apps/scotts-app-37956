7d27c28a27fdbdddeeeb6cca137a75121003bb27
Andreja Trajkovic <andreja.trajkovic@crowdbotics.com>
2022-12-21 22:55:50 +0100

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native";
-
 export const globalOptions = {
   name: "scotts_app_37956",
   url: "https://scotts_app_37956.botics.co",
   api: "https://scotts_app_37956.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     "copy": "Routes available!"
   }
-}
+};
